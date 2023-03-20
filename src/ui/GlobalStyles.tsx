b5250413bddd18b69f51a06c18c2d5adfb185b63
AnastasiyaCheslavovna <kashlejnastya@mail.ru>
2023-03-20 20:15:45 +0300

    new styles

---
+++ src/ui/GlobalStyles.tsx
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+  *{
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+    border: none;
+  }
+  body {
+   font-family: 'Exo 2', 'san-serif';
+  }
+`;
---
+++ src/ui/index.ts
@@ -0,0 +1,5 @@
+import { Colors } from "./colors";
+import { GlobalStyles } from "./GlobalStyles";
+import { Media } from "./media";
+
+export { GlobalStyles, Colors, Media };
