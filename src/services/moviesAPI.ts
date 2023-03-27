f13529c7fc93070cd13f435f32fa438fbd29a8d6
AnastasiyaCheslavovna <kashlejnastya@mail.ru>
2023-03-27 11:13:47 +0300

    Update moviesAPI.ts

--- src/services/moviesAPI.ts
+++ src/services/moviesAPI.ts
@@ -21,5 +21,76 @@ class MoviesAPI {
     "star",
     "man",
   ];
+ private readonly getRandomWord = (words: string[]) => {
+    const random = (Math.random() * this.words.length) | 0;
+    const randomWord = words[random];
+    return randomWord;
+  };
 
+  public getAll = async () => {
+    const params = {
+      s: this.getRandomWord(this.words),
+    };
+
+    const { data } = await this.API.get<IMoviesResponseAPI>(this.API_KEY, { params });
+
+    return data;
+  };
+
+  public getMoreMovies = async (page: string) => {
+    const params = {
+      s: this.getRandomWord(this.words),
+      page: page,
+    };
+
+    const { data } = await this.API.get<IMoviesResponseAPI>(this.API_KEY, { params });
+
+    return data;
+  };
+
+  public getDetailsByImdbID = async (imdbID: string) => {
+    const params = {
+      i: imdbID,
+    };
+
+    const { data } = await this.API.get<IMovieDetailsAPI>(this.API_KEY, { params });
+
+    return data;
+  };
+
+  public getTrends = async (year: string) => {
+    const params = {
+      s: this.getRandomWord(this.words),
+      y: year,
+    };
+
+    const { data } = await this.API.get<IMoviesResponseAPI>(this.API_KEY, { params });
+
+    return data;
+  };
+
+  public sortedMovies = async (movieTitle: string) => {
+    const params = {
+      s: movieTitle,
+    };
+
+    const { data } = await this.API.get<IMoviesResponseAPI>(this.API_KEY, {
+      params,
+    });
+
+    return data;
+  };
+
+  public sortedMoviesByUserParams = async (values: FilterValue) => {
+    const params = {
+      ...values,
+    };
+
+    const { data } = await this.API.get<IMoviesResponseAPI>(this.API_KEY, { params });
+
+    return data;
+  };
+}
+
+export const moviesAPI = new MoviesAPI();
  
