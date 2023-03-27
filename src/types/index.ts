a483e350d236210e3eba487f0765ef7e7021e489
AnastasiyaCheslavovna <kashlejnastya@mail.ru>
2023-03-27 09:28:02 +0300

    Update index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,22 +1,19 @@
-export interface IMoviesAPIResponse {
-    Search: IMovieAPI[];
-    totalResults: null | string;
-    Response: null | string;
-  }
-  
-  export interface IMovieAPI {
-    Poster: string;
-    Title: string;
-    Type: ContentType;
-    Year: string;
-    imdbID: string;
-  }
-  export interface IMovie {
-    poster: string;
-    title: string;
-    type: ContentType;
-    year: string;
-    imdbID: string;
-  }
-  
-  export type ContentType = "movie" | "series" | "episode";
+import {
+  IMovie,
+  IMovieAPI,
+  IMovieDetails,
+  IMovieDetailsAPI,
+  IMoviesResponseAPI,
+  IOption,
+  FilterValue,
+} from "./types";
+
+export type {
+  IMovie,
+  IMovieAPI,
+  IMovieDetails,
+  IMovieDetailsAPI,
+  IMoviesResponseAPI,
+  IOption,
+  FilterValue,
+};
