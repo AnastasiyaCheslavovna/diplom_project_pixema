4e58f135d485faf38268d9b5c44ef99f29ecd90a
AnastasiyaCheslavovna <kashlejnastya@mail.ru>
2023-04-02 13:55:43 +0300

    Update BurgerMenu.tsx

--- src/components/BurgerMenu/BurgerMenu.tsx
+++ src/components/BurgerMenu/BurgerMenu.tsx
@@ -10,38 +10,3 @@ interface IProps {
   toggle: boolean;
 }
 
-export const BurgerMenu = ({ setToggle, toggle }: IProps) => {
-  return (
-    <AnimatePresence>
-      {toggle && (
-        <Wrapper
-          animate={{ x: 0 }}
-          initial={{ x: 300 }}
-          transition={{ delay: 0.3 }}
-          exit={{ x: 550 }}
-        >
-          <CloseButton onClick={() => setToggle()}>
-            <CloseIcons stroke={Color.White} />
-          </CloseButton>
-          <ToolBar>
-            <CustomLink onClick={() => setToggle()} to={ROUTE.HOME}>
-              <HomeIcon width={24} /> <Title>Home</Title>
-            </CustomLink>
-            <CustomLink onClick={() => setToggle()} to={ROUTE.TRENDS}>
-              <TrendsIcon width={24} />
-              <Title>Trends</Title>
-            </CustomLink>
-            <CustomLink onClick={() => setToggle()} to={ROUTE.FAVORITES}>
-              <FavoritesIcon width={24} />
-              <Title>Favorites</Title>
-            </CustomLink>
-            <CustomLink onClick={() => setToggle()} to={ROUTE.SETTINGS}>
-              <SettingsIcon width={24} />
-              <Title>Settings</Title>
-            </CustomLink>
-          </ToolBar>
-        </Wrapper>
-      )}
-    </AnimatePresence>
-  );
-};
